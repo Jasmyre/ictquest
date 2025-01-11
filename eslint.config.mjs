e3dd0e51963a1aaf47a291e51ae90f1bec44b4f4
Jasmyre <lanuzajasmyreandrei@gmail.com>
2025-01-11 13:23:55 +0800

    Fix: Fixed .eslintignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "node_modules/",
+      "components/ui/"
+    ],
+  },
 ];
 
 export default eslintConfig;
