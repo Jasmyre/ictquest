7795bcd443242630279978264df6f228616cbff4
Jasmyre <lanuzajasmyreandrei@gmail.com>
2025-01-18 23:55:22 +0800

    Fix: Changed the design of lessons

--- app/lessons/[topic]/[subtopic]/[index]/page.tsx
+++ app/lessons/[topic]/[subtopic]/[index]/page.tsx
@@ -1,11 +1,16 @@
+"use client";
+
+import Browser from "@/components/Browser";
+import ButtonChoice from "@/components/ButtonChoice";
+import CodeBlock from "@/components/Code";
 import { CustomProgress } from "@/components/CustomProgress";
-import Index1 from "@/components/lessons/html_basics/intro_to_html/Index1";
-import Index2 from "@/components/lessons/html_basics/intro_to_html/Index2";
-import Index3 from "@/components/lessons/html_basics/intro_to_html/Index3";
-import SubmitButton from "@/components/SubmitButton";
-import Link from "next/link";
-import { ReactNode } from "react";
-import BackButton from "../../../../../components/BackButton";
+import LessonCard from "@/components/LessonCard";
+import { Button } from "@/components/ui/button";
+import { shuffle } from "@/lib/utils";
+import { ArrowLeft, ArrowRight, RotateCcw } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { use, useEffect, useState } from "react";
 import Layout from "../../../../components/layout";
 
 interface LessonContentProps {
@@ -18,7 +23,7 @@ interface LessonContentProps {
         };
         content: {
           type: string;
-          label: string | ReactNode;
+          label: string | React.ReactNode;
           id: number;
         }[];
       }[];
@@ -29,7 +34,7 @@ interface LessonContentProps {
 const lessonContent: LessonContentProps = {
   "html-basics": {
     "intro-to-html": {
-      title: "Intro To Html",
+      title: "Introduction to HTML",
       contents: [
         {
           submit: {
@@ -38,32 +43,74 @@ const lessonContent: LessonContentProps = {
           content: [
             {
               id: 0,
-              type: "element",
-              label: <Index1 />,
+              type: "text",
+              label:
+                "Welcome to the Introduction to HTML! Let's start with the basics.",
             },
-          ],
-        },
-        {
-          submit: {
-            label: "continue",
-          },
-          content: [
             {
               id: 1,
               type: "element",
-              label: <Index2 />,
+              label: (
+                <div>
+                  <p>
+                    HTML stands for HyperText Markup Language. It&apos;s the
+                    standard markup language for creating web pages.
+                  </p>
+                  <Image
+                    src="https://fonts.gstatic.com/s/e/notoemoji/latest/1f916/512.gif"
+                    alt="HTML Structure"
+                    width={400}
+                    height={300}
+                    className="mt-4 rounded-lg shadow-md"
+                  />
+                </div>
+              ),
             },
           ],
         },
         {
           submit: {
-            label: "submit",
+            label: "Next",
           },
           content: [
             {
-              id: 4,
+              id: 2,
+              type: "text",
+              label:
+                "HTML uses tags to define elements. Let's look at a simple example:",
+            },
+            {
+              id: 3,
               type: "element",
-              label: <Index3 />,
+              label: (
+                <CodeBlock language="HTML">
+                  {`<p>This is a paragraph.</p>`}
+                </CodeBlock>
+              ),
+            },
+            {
+              id: 4,
+              type: "text",
+              label:
+                "In this example, <p> is the opening tag, and </p> is the closing tag. The content is between these tags.",
+            },
+          ],
+        },
+        {
+          submit: {
+            label: "Try It!",
+          },
+          content: [
+            {
+              id: 5,
+              type: "text",
+              label:
+                "Now, let's try creating a button using HTML. Can you put the pieces in the correct order?",
+            },
+            {
+              id: 6,
+              type: "element",
+              label: <InteractiveCodeExample />,
             },
           ],
         },
@@ -72,35 +119,110 @@ const lessonContent: LessonContentProps = {
   },
 };
 
-export default async function LessonPage(
-  props: Readonly<{
-    params: Promise<{ topic: string; subtopic: string; index: string }>;
-  }>,
-) {
-  const params = await props.params;
-  const lesson =
-    lessonContent[params.topic as keyof typeof lessonContent]?.[
-      params.subtopic
-    ];
-    
-    if (!lesson) {
-      return <div>Lesson not found</div>;
+const data = {
+  choices: [
+    { label: "<button>", priority: 1 },
+    { label: "Click me!", priority: 2 },
+    { label: "</button>", priority: 3 },
+  ],
+};
+
+const shuffledData = shuffle(data.choices);
+
+function InteractiveCodeExample() {
+  const [code, setCode] = useState("");
+  const [disabledButtons, setDisabledButtons] = useState<string[]>([]);
+
+  const handleClick = (label: string) => {
+    setCode((prevCode) => prevCode + label);
+    setDisabledButtons((prevDisabled) => [...prevDisabled, label]);
+  };
+
+  const handleReset = () => {
+    setCode("");
+    setDisabledButtons([]);
+  };
+
+  return (
+    <div>
+      <CodeBlock language="HTML">{code}</CodeBlock>
+      <div className="flex justify-start mt-2">
+        <Button
+          className="border border-gray-200 dark:border-gray-700 shadow-sm bg-white dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700 text-gray-600 hover:text-900 dark:text-gray-400 dark:hover:text-gray-200"
+          onClick={handleReset}
+          variant="outline"
+          size="sm"
+        >
+          <RotateCcw className="mr-2 h-4 w-4" />
+          Reset
+        </Button>
+      </div>
+      <div className="flex justify-center gap-4 flex-wrap mt-4">
+        {shuffledData.map((choice) => (
+          <ButtonChoice
+            key={choice.label}
+            onClick={() => handleClick(choice.label)}
+            disabled={disabledButtons.includes(choice.label)}
+          >
+            {choice.label}
+          </ButtonChoice>
+        ))}
+      </div>
+      <br />
+      {code === "<button>Click me!</button>" && (
+        <Browser
+          title="Great job! You've created a button in HTML."
+          content={`
+        <button>Click me!</button>
+        `}
+        ></Browser>
+      )}
+      <br />
+      <br />
+    </div>
+  );
+}
+
+export default function LessonPage({
+  params: paramsPromise,
+}: Readonly<{
+  params: Promise<{ topic: string; subtopic: string; index: string }>;
+}>) {
+  const router = useRouter();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  const params = use(paramsPromise);
+  const lesson = lessonContent[params.topic]?.[params.subtopic];
+
+  if (!lesson) {
+    return <div>Lesson not found</div>;
   }
 
   const numberOfContent = lesson.contents.length;
-  
-  if (Number (params.index) >= numberOfContent) {
-    params.index = (numberOfContent - 1).toString();
+  let index = Number(params.index);
+
+  if (index >= numberOfContent) {
+    index = numberOfContent - 1;
   }
 
-  let index = Number(params.index);
-  console.log(index);
-
-  const content = lesson.contents[Number(params.index)];
-
-
+  const content = lesson.contents[index];
   const singleProgress = 100 / numberOfContent;
-  console.log(singleProgress);
+
+  const handleNext = () => {
+    if (index < numberOfContent - 1) {
+      router.push(`/lessons/${params.topic}/${params.subtopic}/${index + 1}`);
+    } else {
+      router.push(`/lessons/${params.topic}`);
+    }
+  };
 
   return (
     <Layout>
@@ -109,38 +231,50 @@ export default async function LessonPage(
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <h1 className="text-3xl font-bold leading-tight text-gray-900 dark:text-white">
               {lesson.title}
-              <br />
-              <CustomProgress
-                initialValue={(singleProgress * index) - singleProgress}
-                finalValue={singleProgress * index}
-                delay={100}
-              />
             </h1>
+            <CustomProgress
+              initialValue={singleProgress * index - singleProgress}
+              finalValue={singleProgress * index}
+              delay={100}
+            />
           </div>
         </header>
-        <main className="min-h-[65vh]">
+        <main className="mt-10">
           <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div className="px-4 py-8 sm:px-0">
-              <div className="prose max-w-none ">
-                {content.content.map((text) => {
-                  return (
-                    <div key={text.id}>
-                      {text.type == "text" ? <p>{text.label}</p> : null}
-                      {text.type == "element" ? <div>{text.label}</div> : null}
+            <LessonCard>
+              <div className="flex flex-col justify-between min-h-[65vh]">
+                <div className="prose dark:prose-invert max-w-none">
+                  {content.content.map((item) => (
+                    <div key={item.id}>
+                      {item.type === "text" && <p>{item.label}</p>}
+                      {item.type === "element" && <div>{item.label}</div>}
                       <br />
                     </div>
-                  );
-                })}
+                  ))}
+                </div>
+                <br />
+                <div className="mt-6 flex justify-between">
+                  <Button
+                    className="border border-gray-200 dark:border-gray-700 shadow-sm bg-white dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700 text-gray-600 hover:text-900 dark:text-gray-400 dark:hover:text-gray-200"
+                    variant="outline"
+                    onClick={() => router.back()}
+                    disabled={index === 0}
+                  >
+                    <ArrowLeft className="mr-2 h-4 w-4" />
+                    Back
+                  </Button>
+                  <Button
+                    onClick={handleNext}
+                    className="bg-indigo-600 hover:bg-indigo-700 text-white dark:bg-indigo-700 dark:hover:bg-indigo-600"
+                  >
+                    {index === numberOfContent - 1
+                      ? "Finish"
+                      : content.submit.label}
+                    <ArrowRight className="ml-2 h-4 w-4" />
+                  </Button>
+                </div>
               </div>
-              <div className="mt-6 flex justify-between">
-                <BackButton>Go Back</BackButton>
-                <Link
-                  href={`/lessons/${params.topic}/${params.subtopic}/${++index}`}
-                >
-                  <SubmitButton label={content.submit.label}></SubmitButton>
-                </Link>
-              </div>
-            </div>
+            </LessonCard>
           </div>
         </main>
       </div>
--- components/Browser.tsx
+++ components/Browser.tsx
@@ -1,18 +1,59 @@
-import { Globe } from "lucide-react";
-import { ReactNode } from "react";
+"use client"
+
+import React, { useEffect, useRef, ReactNode } from "react";
 
 interface BrowserProps {
-  children: ReactNode;
+  children?: ReactNode;
+  title?: string;
+  content?: string;
 }
-const Browser = ({ children }: BrowserProps) => {
+const Browser = ({
+  children = "",
+  title = "Great job!",
+  content = ""
+}: BrowserProps) => {
+  const iframeRef = useRef<HTMLIFrameElement | null>(null);
+  
+    useEffect(() => {
+      if (iframeRef.current) {
+        const doc =
+          iframeRef.current.contentDocument ||
+          iframeRef.current.contentWindow?.document;
+  
+        if (doc) {
+          doc.open();
+          doc.write(`${content}`);
+          doc.close();
+        }
+      }
+    });
+
   return (
-    <div className="w-full rounded-xl border-gray-200 dark:border-gray-700">
-      <header className="flex gap-4 p-2 px-4  border-none bg-indigo-600 hover:bg-indigo-700 text-white dark:bg-indigo-600 dark:hover:bg-indigo-600 rounded-[.75rem_.75rem_0_0] text-primary-foreground">
+    <div className="mt-2 p-2 bg-emerald-500 dark:bg-emerald-800 rounded-lg flex flex-col gap-2">
+      <p className="text-green-100 dark:text-green-200">{title}</p>
+      <div className="w-full rounded-xl border-gray-200 dark:border-gray-700 ">
+        {/* <header className="flex gap-4 p-2 px-4  border-none bg-indigo-600 hover:bg-indigo-700 text-white dark:bg-indigo-600 dark:hover:bg-indigo-600 rounded-[.75rem_.75rem_0_0] text-primary-foreground">
         <Globe />
         <p>Browser</p>
-      </header>
-      <div className="browser p-2 px-4 py-10 min-h-[20vh] border border-gray-300 dark:border-gray-900 bg-gray-100 text-black rounded-[0_0_.75rem_.75rem]">
-        {children}
+      </header> */}
+        {/* <div className="rounded-md p-2 px-4 py-10 min-h-[20vh] border border-gray-300 dark:border-gray-900 bg-gray-100 text-black "> */}
+          <div className="browserCode">{children}</div>
+          <div
+            className="browser"
+            style={{ width: "100%", height: "100%", border: "1px solid #ccc" }}
+          >
+            <iframe
+              title="Browser"
+              ref={iframeRef}
+              style={{
+                width: "100%",
+                height: "100%",
+                border: "none",
+                background: "white",
+              }}
+            />
+          </div>
+        {/* </div> */}
       </div>
     </div>
   );
