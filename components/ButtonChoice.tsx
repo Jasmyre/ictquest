2c155808d29679f35fa62af63a150cb75f85a7b8
Jasmyre <lanuzajasmyreandrei@gmail.com>
2025-02-07 21:58:34 +0800

    Fix: Fixed build error on the previous commit

--- components/ButtonChoice.tsx
+++ components/ButtonChoice.tsx
@@ -1,20 +1,46 @@
-import React, { ReactNode } from "react";
+import Prism from "prismjs";
+import React, { ReactNode, useEffect } from "react";
 import { Button } from "./ui/button";
-import SyntaxHighlighter from "react-syntax-highlighter";
-import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
-const ButtonChoice = ({ children, onClick, disabled, ...props }: { children: ReactNode, disabled: boolean, onClick: (e: React.MouseEvent) => void }) => {
+const ButtonChoice = ({ children, onClick, language = "html", disabled, ...props }: { language: string, children: ReactNode, disabled: boolean, onClick: (e: React.MouseEvent) => void }) => {
+
+  useEffect(() => {
+    Prism.highlightAll();
+  })
+  
+  const escapeHtml = (unsafe: string) => {
+    return unsafe
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;");
+  };
+  
   return (
     <Button
-    disabled={disabled}
-    onClick={onClick}
+      disabled={disabled}
+      onClick={onClick}
       variant={"outline"}
-      className="border px-6 py-2 border-gray-200 dark:border-gray-700 bg-accent bg-[#282C34] hover:bg-[#282C34] hover:brightness-200 hover:dark:brightness-125  rounded text-gray-200 max-md:text-white text-accent-foreground"
+      className="rounded border border-gray-200 bg-gray-700 px-6 py-2 text-accent-foreground text-gray-200 hover:bg-[#282C34] hover:brightness-200 dark:border-gray-700 hover:dark:brightness-125 max-md:text-white"
       {...props}
     >
-      <SyntaxHighlighter language="html" style={atomOneDark}>
-        {children as string}
-      </SyntaxHighlighter>
+      <pre
+        style={{
+          margin: 0,
+          padding: ".5rem 1rem",
+          border: "none",
+          background: "initial",
+        }}
+        className="m-0 p-2 "
+      >
+        <code
+          className={`language-${language} m-0 h-full min-h-[20vh] rounded-[0_0_.75rem_.75rem] border-none bg-[#282C34]`}
+          dangerouslySetInnerHTML={{
+            __html: escapeHtml(children as string).trim(),
+          }}
+        />
+      </pre>
     </Button>
   );
 };
