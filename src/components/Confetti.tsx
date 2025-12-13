26a110fd1b7244f3266160df06d51dd7c76031ce
Jasmyre <lanuzajasmyreandrei@gmail.com>
2025-12-13 15:18:42 +0800

    chore: add blank line for improved readability in Confetti component

--- src/components/Confetti.tsx
+++ src/components/Confetti.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 
 const Confetti = () => {
   const [showConfetti, setShowConfetti] = useState(false);
+
   useEffect(() => {
     setShowConfetti(true);
   }, []);
